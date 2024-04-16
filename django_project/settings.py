a987d056b49171038b368250c3072589b52bb0a9
jaytech504 <jasonyakubu543@gmail.com>
2024-04-16 12:47:06 +0100

    password reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -134,3 +134,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jasonyakubu543@gmail.com'
+EMAIL_HOST_PASSWORD = 'Gynjm100'
